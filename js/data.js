1a72e25aa73e255bceb1c77b6d1326394fb78bce
congcccong <302666299@qq.com>
2019-01-23 22:07:09 +0800

    抽奖实例
    抽奖

---
+++ js/data.js
@@ -0,0 +1,91 @@
+    var personArray = new Array;
+
+personArray.push({id: 1,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "刘月刚",prize: 0});
+personArray.push({id: 2,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "李戈",prize: 0});
+personArray.push({id: 3,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "杨欢欢",prize: 0});
+personArray.push({id: 4,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "杜杰颖",prize: 0});
+personArray.push({id: 5,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "蔡进",prize: 0});
+personArray.push({id: 6,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "李娜",prize: 0});
+personArray.push({id: 7,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "崔婕",prize: 0});
+personArray.push({id: 8,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "梁爽",prize: 0});
+personArray.push({id: 9,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "张梓枫",prize: 0});
+personArray.push({id: 10,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "赵建立",prize: 0});
+personArray.push({id: 11,image :"img/tx2.png",thumb_image :"img/tx2.png",name: "姜敏敏",prize: 0});
+personArray.push({id: 12,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "张少堃",prize: 0});
+personArray.push({id: 13,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "赵占轩",prize: 0});
+personArray.push({id: 14,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "张颖南",prize: 0});
+personArray.push({id: 15,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "赵磊",prize: 0});
+personArray.push({id: 16,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "魏子盛",prize: 0});
+personArray.push({id: 17,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "王亚楠",prize: 0});
+personArray.push({id: 18,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "马秀辉",prize: 0});
+personArray.push({id: 19,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "朱彦霖",prize: 0});
+personArray.push({id: 20,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "石庆华",prize: 0});
+personArray.push({id: 21,image :"img/tx2.png",thumb_image :"img/tx2.png",name: "赵炳林",prize: 0});
+personArray.push({id: 22,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "邵真真",prize: 0});
+personArray.push({id: 23,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "汤轶峰",prize: 0});
+personArray.push({id: 24,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "马占成",prize: 0});
+personArray.push({id: 25,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "王东亮",prize: 0});
+personArray.push({id: 26,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "张瑞亭",prize: 0});
+personArray.push({id: 27,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "马占良",prize: 0});
+personArray.push({id: 28,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "孙宗方",prize: 0});
+personArray.push({id: 29,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "刘春来",prize: 0});
+personArray.push({id: 30,image :"img/tx2.png",thumb_image :"img/tx2.png",name: "王建权",prize: 0});
+personArray.push({id: 31,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "刘兴旭",prize: 0});
+personArray.push({id: 32,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "马云浩",prize: 0});
+personArray.push({id: 33,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "刘跃凯",prize: 0});
+personArray.push({id: 34,image :"img/tx2.png",thumb_image :"img/tx2.png",name: "裴少林",prize: 0});
+personArray.push({id: 35,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "郭庆越",prize: 0});
+personArray.push({id: 36,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "毕延辉",prize: 0});
+personArray.push({id: 37,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "庞传智",prize: 0});
+personArray.push({id: 38,image :"img/tx1.png",thumb_image :"img/tx1.png",name: "符米亮",prize: 0});
+
+
+    
+        // personArray.push({
+        //     id: 546,
+        //     image :"img/tx.png",
+        //     thumb_image :"img/tx.png",
+        //     name: "老大",
+        //     prize: 0
+        // });
+
+
+    
+        // personArray.push({
+        //     id: 722,
+        //     image :"img/tx1.png",
+        //     thumb_image :"img/tx1.png",
+        //     name: "老二",
+        //     prize: 0
+        // });
+
+
+    
+        // personArray.push({
+        //     id: 1044,
+        //     image :"img/tx2.png",
+        //     thumb_image :"img/tx2.png",
+        //     name: "老三",
+        //     prize: 0
+        // });
+
+
+    
+        // personArray.push({
+        //     id: 1051,
+        //     image :"img/tx3.png",
+        //     thumb_image :"img/tx3.png",
+        //     name: "老四",
+        //     prize: 0
+        // });
+
+
+    
+        // personArray.push({
+        //     id: 1711,
+        //     image :"img/tx4.png",
+        //     thumb_image :"img/tx4.png",
+        //     name: "老五",
+        //     prize: 0
+        // });
+        // personArray.push({id: 1711,image :"img/tx4.png",thumb_image :"img/tx4.png",name: "老五",prize: 0});
